72dc7741933bc1b0a6e6916ab3841555a4cef412
hzxrosydawn <hzxrosydawn@gmail.com>
2017-02-04 10:00:36 +0800

    添加一个9Patch图片与普通Png图片拉伸对比界面

---
+++ app/src/main/java/com/example/vincenthuang/androiddemos/activity/NinePatchTest.java
@@ -0,0 +1,15 @@
+package com.example.vincenthuang.androiddemos.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vincenthuang.androiddemos.R;
+
+public class NinePatchTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nine_patch_test);
+    }
+}
